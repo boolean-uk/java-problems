65b823db524064a024db30cbfe1807988b6fee53
Dave Ames <david.john.ames@gmail.com>
2023-04-14 13:13:13 +0100

    Changed `assertEquals()` to `assertArrayEquals()` in each case.

--- src/test/java/com/booleanuk/easy/MultiplesTest.java
+++ src/test/java/com/booleanuk/easy/MultiplesTest.java
@@ -7,8 +7,8 @@ public class MultiplesTest {
     @Test
     public void shouldReturnMultiples() {
         Multiples problem = new Multiples();
-        Assertions.assertEquals(new int[]{7, 14, 21, 28, 35}, problem.solution(7, 5));
-        Assertions.assertEquals(new int[]{12, 24, 36, 48, 60, 72, 84, 96, 108, 120}, problem.solution(12, 10));
-        Assertions.assertEquals(new int[]{17, 34, 51, 68, 85, 102}, problem.solution(17, 6));
+        Assertions.assertArrayEquals(new int[]{7, 14, 21, 28, 35}, problem.solution(7, 5));
+        Assertions.assertArrayEquals(new int[]{12, 24, 36, 48, 60, 72, 84, 96, 108, 120}, problem.solution(12, 10));
+        Assertions.assertArrayEquals(new int[]{17, 34, 51, 68, 85, 102}, problem.solution(17, 6));
     }
 }
